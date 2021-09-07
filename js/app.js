d99aaf49a890283e3cd799fb308b1a30c29bc168
Robert Mattison <51356300+rmattison@users.noreply.github.com>
2021-09-07 03:12:57 -0500

    Added event listeners to voting buttons.

--- js/app.js
+++ js/app.js
@@ -2,28 +2,18 @@ const form = document.querySelector("#post-form");
 const content = document.querySelector("#content-container");
 const titleInput = document.getElementById('title-input')
 const descInput = document.getElementById('desc-input')
+const authorInput = document.getElementById('author-input')
 const submitButton = document.getElementById('submit-btn')
 const postsCotainer = document.querySelector('.posts-container')
 
-
-
-
-
-//Load all event listeners
-function loadEventListeners() {
-  form.addEventListener('submit', addNewPost);
-}
-
-//Load all event listeners
-loadEventListeners()
-
-
-function addNewPost(e) {
+// Submit event for New Post Submission
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const newPostData = {
     title: titleInput.value,
-    description: descInput.value
+    description: descInput.value,
+    posted_by: authorInput.value
   }
 
   post("https://localhost:44366/api/posts", newPostData)
@@ -32,33 +22,68 @@ function addNewPost(e) {
 
   titleInput.value = ""
   descInput.value = ""
+  authorInput.value = ""
 
   alert("Submitted successfully!");
+  window.location.reload();
+});
+
+//Load all event listeners
+function loadEventListeners() {
+  // Click event for upvote buttons
+  document.querySelectorAll('.upvote-btn').forEach(item => {
+    item.addEventListener('click', (e) => {
+      let postId = e.target.parentElement.parentElement.parentElement.parentElement.id
+
+      const newVote = {
+        up: true
+      }
+
+      put(`https://localhost:44366/api/posts/${postId}/vote`, newVote)
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+    })
+  })
+
+
+  // Click event for downvote buttons
+  document.querySelectorAll('.downvote-btn').forEach(item => {
+    item.addEventListener('click', (e) => {
+      let postId = e.target.parentElement.parentElement.parentElement.parentElement.id
+
+      const newVote = {
+        up: false
+      }
+
+      put(`https://localhost:44366/api/posts/${postId}/vote`, newVote)
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+    })
+  })
 }
 
-
-
+// Load posts onto the screen
 get("https://localhost:44366/api/posts")
-  .then((data) => loadInitialPosts(data))
+  .then((data) => loadPosts(data))
   .catch((err) => console.log(err));
 
+//Load all event listeners
+loadEventListeners()
 
 
-
+// Make an HTTP GET Request
 async function get(url) {
   const response = await fetch(url);
   const resData = await response.json();
   return resData;
 }
 
-  // Make an HTTP POST Request
+// Make an HTTP POST Request
 async function post(url, data) {
   const res = await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-
-
     },
     body: JSON.stringify(data),
   });
@@ -67,25 +92,58 @@ async function post(url, data) {
   return resData;
 }
 
+// Make an HTTP PUT Request
+async function put(url, data) {
+  const res = await fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
 
-function loadInitialPosts(data) {
+  const resData = await res.json();
+
+  return resData;
+}
+
+
+// Loads all posts onto the screen
+function loadPosts(data) {
   for (i = data.length-1; i >= 0; i--) {
-    let text = `<div class="post">
+
+    let postDate = new Date(data[i].time_posted)
+    let todaysDate = new Date()
+
+    let outputTime = ""
+
+    if(postDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
+      outputTime = new Date(data[i].time_posted).toLocaleTimeString('en-US', { hour12: true, hour: "numeric", minute: "numeric"});
+    } else {
+      outputTime = data[i].time_posted.split(' ')[0]
+    }
+
+    let text = `<div class="post" id="${data[i].id}">
     <div class="vote">
+    <a class="upvote-btn" href="#">
     <div class="upvote">
       <i class="fas fa-arrow-up fa-lg"></i>
     </div>
-    <div class="number"><p>${data[i].upvotes}</p></div>
-    <div class="downvote">
-      <i class="fas fa-arrow-down fa-lg"></i>
-    </div>
+    </a>
+    
+    <div class="number"><p>${data[i].upvotes - data[i].downvotes}</p></div>
+      <a class="downvote-btn" href="#">
+        <div class="downvote">
+          <i class="fas fa-arrow-down fa-lg"></i>
+        </div>
+      </a>
     </div>
     <div class="thumb"><img class="thumb" src="./img/txt-file.png"></div>
     <div class="flex-container">
     <a href="#"><h2 class="title">${data[i].title}</h2></a>
     <p class="description">${data[i].description}</p>
     <p class="time">
-      posted ${data[i].time_posted.split(' ')[0]} by <a href="" class="username">${data[i].posted_by}</a>
+      posted ${outputTime} by <a href="" class="username">${data[i].posted_by}</a>
     </p>
     <div class="links">
       <a href=""> comments</a><a href="">save</a><a href="">hide</a><a href="">report</a><a href="">block</a>
@@ -95,6 +153,7 @@ function loadInitialPosts(data) {
 
     content.innerHTML += text;
   }
+  loadEventListeners()
 }
 
 
